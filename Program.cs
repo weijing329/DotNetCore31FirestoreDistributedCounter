1dade4a0a87d59d1c6ae57d98d8f1e79500feb05
Wayne 王維靖 <weijing329@outlook.com>
2020-12-02 10:29:22 +0800

    dotnet new console / gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore31FirestoreDistributedCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
